9cbd85538bdcdb429ca8fe19f9c820a641043956
VTellesRg <vini.m.telles@gmail.com>
2024-07-20 12:40:59 -0300

    add: first settings JS, html, css

---
+++ jogo-js/index.js
@@ -0,0 +1,9 @@
+const cat = document.querySelector('.cat');
+const start = document.querySelector('.start');
+const gameOver = document.querySelector('.gameOver');
+const pontos = document.querySelector('.pontos');
+const gameBase = document.querySelector('.gameBase');
+const background = document.querySelector('.background');
+
+const pointAudio = document.querySelector('Audio1');
+const dieAudio = document.querySelector('Audio2');
